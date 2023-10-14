fdc03f6a5f6cc68db2412de2730f3752f3c2928e
vanya-beat <vanyabeat@yandex.ru>
2023-10-14 09:54:50 +0300

    Update testsg

--- example_test.cpp
+++ example_test.cpp
@@ -1,10 +1,10 @@
 #include <gtest/gtest.h>
 #include <string>
-TEST(GoogleTestCi, Foo) {
-  ASSERT_TRUE(true);
+
+TEST(TestName_Other, Foo3) {
+    ASSERT_TRUE(true);
 }
-TEST(ExampleTest, DmitriiLeonidovich){
-    std::string dml = "DmitriiLeonidovich";
-    const char * dml_c = "DmitriiLeonidovich";
-    ASSERT_STREQ(dml.c_str(), dml_c);
+
+TEST(TestName_Other3, Foo3) {
+    ASSERT_TRUE(true);
 }
