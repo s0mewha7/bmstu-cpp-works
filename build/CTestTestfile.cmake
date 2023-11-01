92cdffdcb8aa02843fe2a9afcd6885db9c349d57
s0mewha7 <samit.zeinalli@yandex.ru>
2023-11-02 01:13:19 +0300

    works

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/somewhat/bmstu-works
-# Build directory: /home/somewhat/bmstu-works/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/somewhat/bmstu-works/build/bmstu-works_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
