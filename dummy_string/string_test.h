edaaf44931fbf5680572d67416a5fc096c7e6b0e
s0mewha7 <samit.zeinalli@yandex.ru>
2023-11-26 19:00:48 +0300

    change

--- dummy_string/string_test.h
+++ dummy_string/string_test.h
@@ -6,6 +6,7 @@
 #define DUMMY_STRING_STRING_TEST_H_
 
 #include "basic_string.h"   // Заголовочные файлы текущего проекта
+#include "string_test.h"
 #include <gtest/gtest.h>    // Заголовочные файлы Google Test
 #include <cstddef>           // Заголовочные файлы стандартной библиотеки C
 #include <iostream>          // Заголовочные файлы стандартной библиотеки C++
