4a4cceedeb19290065626992091f2914aa463a83
s0mewha7 <samit.zeinalli@yandex.ru>
2023-11-27 10:58:43 +0300

    change

--- let2-dec2oct/dec2oct_test.cpp
+++ let2-dec2oct/dec2oct_test.cpp
@@ -1,24 +1,21 @@
 #include <gtest/gtest.h>
 #include "fromdec2oct.h"
 
-TEST(ConvertToOctalStringTest, PositiveNumber)
-{
+TEST(ConvertToOctalStringTest, PositiveNumber) {
     int num1 = 666;
     char *result1 = convertToOctalString(num1);
     EXPECT_STREQ(result1, "0o1232");
     delete[] result1;
 }
 
-TEST(ConvertToOctalStringTest, NegativeNumber)
-{
+TEST(ConvertToOctalStringTest, NegativeNumber) {
     int num2 = -666;
     char *result2 = convertToOctalString(num2);
     EXPECT_STREQ(result2, "-0o1232");
     delete[] result2;
 }
 
-TEST(ConvertToOctalStringTest, Zero)
-{
+TEST(ConvertToOctalStringTest, Zero) {
     int num3 = 0;
     char *result3 = convertToOctalString(num3);
     EXPECT_STREQ(result3, "0o0");
