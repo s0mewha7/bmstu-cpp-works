d143265b16572142d4c6b7105104a440707aa395
s0mewha7 <samit.zeinalli@yandex.ru>
2023-11-27 10:26:09 +0300

    change

--- let5-JacquesFresco/bruteforce.cpp
+++ let5-JacquesFresco/bruteforce.cpp
@@ -1,30 +1,23 @@
 #include "function.h"
 
-void generatePasswords(const std::string &charset, int length, std::string currentPassword)
-{
-    if (length == 0)
-    {
+void generatePasswords(const std::string &charset, int length, std::string currentPassword) {
+    if (length == 0) {
         std::cout << currentPassword << std::endl;
         return;
     }
-    for (const auto &c : charset)
-    {
+    for (const auto &c : charset) {
         generatePasswords(charset, length - 1, currentPassword + c);
     }
 }
 
-void generated_password()
-{
+void generated_password() {
     std::ofstream outputFile("passwords.txt");
 
-    if (outputFile.is_open())
-    {
+    if (outputFile.is_open()) {
         generatePasswords(charset, passwordLength, "");
 
         outputFile.close();
-    }
-    else
-    {
+    } else {
         std::cerr << "File doesn't exists" << std::endl;
     }
 }
